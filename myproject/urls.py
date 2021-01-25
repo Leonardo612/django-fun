f4aaff2aace90f1633215bbdc75252b145b8aad5
LEONARDO DA SILVA KOSTETZER <leonardo.kostetzer@alunos.sc.senac.br>
2021-01-25 17:18:24 -0300

    25/01

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from accounts import views as accounts_views
+
 from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pokemaoc/', include('pokemaoc.urls')),
-    url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r"^", include("users.urls")),
+    
 ]
